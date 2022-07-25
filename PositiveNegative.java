17480b88be48ecd4eaf0bfd1b3c9595e27faa1db
Arshad1803 <arshadg390@gmail.com>
2022-07-25 14:05:31 +0530

    Acitivity

---
+++ PositiveNegative.java
@@ -0,0 +1,17 @@
+// Program to check the number is Positive or Negative
+
+class PositiveNegative
+{
+	public static void main(String args[])
+	{
+		int number = 12;
+		if(number > 0)
+		{
+			System.out.println("The number is Positive");
+		}
+		else
+		{
+			System.out.println("The number is Negative");
+		}
+	}
+}
